238ddb71498de1aae0ed3d6572a6a54de718249b
Polina Golubtsova <55252542+polina4265@users.noreply.github.com>
2021-03-18 17:59:57 +0300

    Add files via upload
    Заготовка игры «Пинг-понг»: игровая сцена, залитая цветом. Выход из игры при нажатии на кнопку «Закрыть окно».

---
+++ ping_pong_main.py
@@ -0,0 +1,20 @@
+from pygame import *
+
+#Игровая сцена:
+back = (200, 255, 255) # цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+clock = time.Clock()
+FPS = 60
+game = True
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
