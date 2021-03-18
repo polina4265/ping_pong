85517e92226523b8f73e3e3d383ea5f9d0302a7e
Polina Golubtsova <55252542+polina4265@users.noreply.github.com>
2021-03-18 18:15:00 +0300

    Update ping_pong_main.py
    Добавлен класс GameSprite. Введён новый спрайт ball. При запуске игры мяч отображается на сцене.

--- ping_pong_main.py
+++ ping_pong_main.py
@@ -1,5 +1,22 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height): # добавить еще два параметра при создании и задавать размер прямоугольгника для картинки самим
+        super().__init__()
+ 
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height)) # вместе 55,55 - параметры
+        self.speed = player_speed
+ 
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 #Игровая сцена:
 back = (200, 255, 255) # цвет фона (background)
 win_width = 600
@@ -11,10 +28,14 @@ clock = time.Clock()
 FPS = 60
 game = True
 
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    ball.reset()
 
     display.update()
     clock.tick(FPS)
